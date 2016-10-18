d0b1e08bc02b6e52443b99f9b2ac7cbd54e71bdb
Mike Grazier <michael.grazier@gmail.com>
2016-10-18 16:17:07 -0500

    Added obstacle generation
    Added obstacles at random intervals and began work on randomizing
    obstacle height.

--- Assets/Scripts/FlappyController.cs
+++ Assets/Scripts/FlappyController.cs
@@ -5,12 +5,14 @@ public class FlappyController : MonoBehaviour {
 
 	public Rigidbody rb;
 	public float startVelX = 1.0f;
+    public float gravity = -10.0f;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 
 		rb.velocity = new Vector3 (startVelX, 0f, 0f);
+        Physics.gravity = new Vector3(0, gravity, 0);
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObstacleSpawner : MonoBehaviour {
+
+    public GameObject obstacle;
+    public GameObject obstacleSpawner;
+    public GameObject flappy;
+
+    public List<Transform> childObstacles;
+
+    private float spawnDelay;   // Time between spawns
+    private float lastSpawn;
+    private float nextSpawn;    
+    private Vector3 newSpawnerPosition;
+
+    private GameObject newObstacle;
+    private float bottomObstacleScaleY;
+    private float topObstaclePositionY;
+    private float topObstacleScaleY;
+    private float bottomObstaclePositionY;
+
+    // Use this for initialization
+    void Start () {
+        obstacleSpawner = GameObject.Find("ObstacleSpawner");
+        Instantiate(obstacle, obstacleSpawner.transform.position, Quaternion.identity);
+
+        spawnDelay = Random.Range(2f, 3.5f);
+        nextSpawn = Time.time + spawnDelay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        newSpawnerPosition = new Vector3(flappy.transform.position.x, 0f, 0f);
+        newSpawnerPosition.x += 12;
+        obstacleSpawner.transform.position = newSpawnerPosition;
+
+        if (nextSpawn - Time.time <= 0) {
+            
+            newObstacle = (GameObject)Instantiate(obstacle, obstacleSpawner.transform.position, Quaternion.identity);
+            newObstacle.GetComponentsInChildren<Transform>(childObstacles);
+            
+
+            childObstacles[0].localScale.Set(1f, bottomObstacleScaleY, 1f);
+            childObstacles[1].localScale.Set(1f, topObstacleScaleY, 1f);
+
+            childObstacles[0].localPosition.Set(1, bottomObstaclePositionY, 1f);
+            childObstacles[1].localPosition.Set(1, topObstaclePositionY, 1f);
+
+            spawnDelay = Random.Range(2f, 3.5f);
+            nextSpawn = Time.time + spawnDelay;
+        }
+	}
+}
